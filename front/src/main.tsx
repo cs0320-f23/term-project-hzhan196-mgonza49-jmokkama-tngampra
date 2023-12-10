b8d2575df8ba3febfbf30d0a1171dc717891be81
Hannah Zhang <hz2435@gmail.com>
2023-12-09 19:17:06 -0500

    added routing 2

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App.tsx'
 import './style/index.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from './components/Homepage.tsx';
