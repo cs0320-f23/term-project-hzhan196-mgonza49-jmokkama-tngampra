93570aec3029318cdc101e0da53f0205165a1c47
Francis <miguel_francisco_gonzalez@brown.edu>
2023-12-12 21:35:56 -0500

    feat: google authentication

--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { Fragment, useState } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { BrowserRouter, Link} from 'react-router-dom';
+import { BrowserRouter, Link } from "react-router-dom";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import "../style/interface.css";
-import "../style/App.css"
-
+import "../style/App.css";
+import { Login } from "./Login";
 
 const navigation = [
   { name: "Home", href: "/", current: true },
@@ -13,90 +13,100 @@ const navigation = [
 ];
 
 export default function Navbar() {
+  const handleSignIn = async () => {
+    try {
+      const loginStatus = (await Login()).toString();
+      console.log("login status " + loginStatus);
+      if (loginStatus === "success") {
+        console.log("it worked!");
+      }
+    } catch (error) {
+      console.log("error in onClick");
+      console.error(error);
+    }
+  };
 
   return (
     //    <h1>NavBar</h1>
     <Disclosure as="nav" className="menu">
-    {({ open }) => (
-      <>
-        <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-          <div className="nav2">
-            {/* onClick={() => router.push("./")} */}
-            Study Abroad @ Brown
+      {({ open }) => (
+        <>
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="nav2">
+              {/* onClick={() => router.push("./")} */}
+              Study Abroad @ Brown
+            </div>
           </div>
-        </div>
-     
 
-        <div>
-          {/* flex space-x-4 */}
-          {navigation.map((item) => (
+          <div>
+            {/* flex space-x-4 */}
+            {navigation.map((item) => (
+              <Link
+                key={item.name}
+                to={item.href}
+                className="nav-item"
+                aria-current={item.current ? "page" : undefined}
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
 
-            <Link
-              key={item.name}
-              to={item.href}
-              className="nav-item"
-              aria-current={item.current ? "page" : undefined}
+          <Menu as="div">
+            {/* <div className="profile-outer"> */}
+            <Menu.Button
+              className="ml-2 relative flex rounded-full custom-dark-gray focus:outline-none"
+              // style={{ background: "transparent", border: "none" }}
             >
-              {item.name}
-            </Link>
-          ))}
-        </div>
+              <img
+                className="profile-circle"
+                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+            </Menu.Button>
 
-        <Menu as="div">
-          {/* <div className="profile-outer"> */}
-          <Menu.Button className="ml-2 relative flex rounded-full custom-dark-gray focus:outline-none"
-            // style={{ background: "transparent", border: "none" }}
-          >
-            <img
-              className="profile-circle"
-              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt=""
-            />
-          </Menu.Button>
-
-
-          <Transition
-            as= {Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
-          >
-            <Menu.Items className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-opacity-5 focus:outline-none">
-              {/* <div className=""> */}
-                  <Menu.Item>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-opacity-5 focus:outline-none">
+                {/* <div className=""> */}
+                <Menu.Item>
                   {({ active }) => (
-                      <button
-                        className={`${
-                        active ? 'bg-gray-200' : ''
+                    <button
+                      className={`${
+                        active ? "bg-gray-200" : ""
                       } w-full text-left block px-4 py-2 text-gray-700`}
                       // href="/settings"
-                      >
+                    >
                       Settings
-                      </button>
+                    </button>
                   )}
-                  </Menu.Item>
+                </Menu.Item>
 
-                  <Menu.Item>
+                <Menu.Item>
                   {({ active }) => (
-                      <button
-                      // onClick={handleSignOut}
+                    <button
+                      onClick={handleSignIn}
                       className={`${
-                        active ? 'bg-gray-200' : ''
+                        active ? "bg-gray-200" : ""
                       } w-full text-left block px-4 py-2 text-gray-700`}
-                      >
+                    >
                       Sign Out
-                      </button>
+                    </button>
                   )}
-                  </Menu.Item>
-              {/* </div> */}
-            </Menu.Items>
-          </Transition>
-        </Menu>
-      </>
-    )}
-  </Disclosure>
+                </Menu.Item>
+                {/* </div> */}
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </>
+      )}
+    </Disclosure>
   );
 }
---
+++ front/src/components/firebaseConfig.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA1GK-oSRTExl0-wuGiwT9myKrDj7a-tu0",
+  authDomain: "study-abroad-8f214.firebaseapp.com",
+  projectId: "study-abroad-8f214",
+  //   storageBucket: "study-abroad-8f214.appspot.com",
+  //   messagingSenderId: "1025997861613",
+  appId: "1:1025997861613:web:6050f3c650b44a8cb592e4",
+};
+
+export default firebaseConfig;
